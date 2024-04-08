d0e5d2613b07345448afbff79b3615eeb6dc67e3
ValeriyCh04 <164645172+ValeriyCh04@users.noreply.github.com>
2024-04-08 11:59:34 +0300

    Add files via upload

---
+++ homework8.py
@@ -0,0 +1,8 @@
+list_ = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+for i in range(len(list_)):
+    print("Я езжу на автомабиле марки: ", list_[i])
+
+cars_count = 0
+for i in range(1):
+    for j in range(1,11):
+        print(f'{i} + {j} = {i + j}')
